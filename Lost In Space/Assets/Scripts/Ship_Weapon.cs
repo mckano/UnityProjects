49bdc3e54357a0de6c1557642a0e56bea67620df
Luis McKinnon <45960722+mckano@users.noreply.github.com>
2019-12-28 00:23:54 -0600

    Changes
    Ship{
    Movement,
    Shooting}
    Bullet{
    Movement,
    Destroy}

---
+++ Lost In Space/Assets/Scripts/Ship_Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Weapon : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject shootingPosition;
+
+    public float bulletRatio;
+    float shootingTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootingTime = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootingTime += Time.deltaTime;
+
+        if(Input.GetButton("Fire1"))
+        {
+            if(shootingTime >= bulletRatio)
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, shootingPosition.transform.position, shootingPosition.transform.rotation);
+        shootingTime = 0f;
+    }
+}
